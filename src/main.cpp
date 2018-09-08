85972ad3f339862af70a280ae63a3021c68f0c04
Dimitri Belopopsky <dimitri@belopopsky.com>
2018-09-08 15:20:48 +0200

    Basic tilemap collision resolution

--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,7 @@ namespace constants {
   constexpr int object_height{64};
 }
 
-enum class TextureType : int { Player = 1, Wall = 2, Box, Ground, Goal };
-
-struct GameObject
-{ 
-  TextureType type;
-  Vec3 colour;
-  Vec4 rect{0, 0, 64, 64};
-};
+enum class TextureType : int { Player = 1, Wall, Box, Goal, Ground };
 
 std::unordered_map<TextureType, sdl2::sdl_texture_ptr> buildTextures(sdl2::sdl_renderer_ptr& renderer)
 {
@@ -54,16 +47,47 @@ std::unordered_map<TextureType, sdl2::sdl_texture_ptr> buildTextures(sdl2::sdl_r
   return textures;
 }
 
-struct Level {
-  int level_width;
-  int level_height;
-  std::string objects;
+struct Player
+{
+  TextureType tex;
+  Vec4 rect{0, 0, 64, 64};
 };
 
-Level level1{8, 3,
-	     "22222222"
-	     "20103052"
-	     "22222222"};
+enum class LevelObject
+  {
+   Player = 1,
+   Wall,
+   Box,
+   Goal,
+   Unknown
+  };
+
+struct Level {
+  Level(int levelWidth, int levelHeight, std::string levelDescription)
+    : width(levelWidth), height(levelHeight)
+  {
+    for (auto object : levelDescription) {
+      level.emplace_back(static_cast<LevelObject>(std::atoi(&object)));
+    }
+  }
+  
+  int width;
+  int height;
+  std::vector<LevelObject> level;
+
+  Vec2 findPlayerPosition() {
+    for (std::size_t i = 0; i < width; ++i) {
+      for (std::size_t j = 0; j < height; ++j) {
+	if (level[j * width + i] == LevelObject::Player) {
+	  std::cout << "i " << i << " j " << j << '\n';
+	  return {i, j};
+	}
+      }
+    }
+    return {-1, -1};
+  }
+};
+
 
 int main()
 {
@@ -80,15 +104,19 @@ int main()
  
   auto renderer = sdl2::make_renderer(window);
   
-  auto textures = buildTextures(renderer); 
+  auto textures = buildTextures(renderer);
 
-  GameObject player;
-  player.rect.x = constants::object_width;
-  player.rect.y = constants::object_height;
 
-  GameObject box;
-  box.rect.x = constants::object_width*2;
-  box.rect.y = constants::object_height;
+
+  Level level{8, 3,
+	      "22222222"
+	      "20103042"
+	      "22222222"};
+  
+  Player player;
+  Vec2 playerStart = level.findPlayerPosition();
+  player.rect.x = playerStart.x * constants::object_width;
+  player.rect.y = playerStart.y * constants::object_height;
   
   SDL_Event event;
   bool running = true;
@@ -99,17 +127,26 @@ int main()
 	running = false;
       } break;
       case SDL_KEYDOWN: {
+	int player_x = static_cast<int>(player.rect.x) / constants::object_width;
+	int player_y = static_cast<int>(player.rect.y) / constants::object_height;
+
+	std::cout << player_x << ' ' << player_y << '\n';
+	
 	switch (event.key.keysym.sym) {
 	case SDLK_UP: {
+	  if (level.level[(player_y - 1) * level.width + (player_x)] != LevelObject::Wall)
 	  player.rect.y -= 64;
 	} break;
 	case SDLK_DOWN: {
+	  if (level.level[(player_y + 1) * level.width + (player_x)] != LevelObject::Wall)
 	  player.rect.y += 64;	  
 	} break;
 	case SDLK_LEFT:{
+	  if (level.level[(player_y) * level.width + (player_x - 1)] != LevelObject::Wall)
 	  player.rect.x -= 64;
 	} break;
 	case SDLK_RIGHT:{
+	  if (level.level[(player_y) * level.width + (player_x + 1)] != LevelObject::Wall)
 	  player.rect.x += 64;
 	} break;
 	case SDLK_ESCAPE:{
@@ -122,48 +159,50 @@ int main()
     
     sdl2::clear(renderer);
 
+    sdl2::copyToRenderer(renderer, textures[TextureType::Ground], {0, 0, 0, 0});
+    
     glm::vec2 position{0, 0};
 
-    for (auto object : level1.objects) {
-      switch (static_cast<TextureType>(std::atoi(&object)))
+    for (auto object : level.level) {
+      switch (object)
 	{
-	case TextureType::Wall:
+	case LevelObject::Wall:
 	  sdl2::copyToRenderer(renderer, textures[TextureType::Wall], {static_cast<int>(position.x),
 								       static_cast<int>(position.y),
 								       constants::object_width,
 								       constants::object_height});
 	  break;
-	case TextureType::Player:
-	  sdl2::copyToRenderer(renderer, textures[TextureType::Player], {static_cast<int>(player.rect.x),
-									 static_cast<int>(player.rect.y),
-									 static_cast<int>(player.rect.z),
-									 static_cast<int>(player.rect.w)});
+	case LevelObject::Player:
+	  // done afterwards so that player always looks above some of the objects
 	  break;
-	case TextureType::Box:
+	case LevelObject::Box:
 	  sdl2::copyToRenderer(renderer, textures[TextureType::Box], {static_cast<int>(position.x),
 								       static_cast<int>(position.y),
 								       constants::object_width,
 								       constants::object_height});
 	  break;
-	case TextureType::Goal:
+	case LevelObject::Goal:
 	  sdl2::copyToRenderer(renderer, textures[TextureType::Goal], {static_cast<int>(position.x),
 								       static_cast<int>(position.y),
 								       constants::object_width,
 								       constants::object_height});
 	  break;
 	default:
-	  std::cout << "toto\n";
-	  break;
-	  
-      }
+	  break;	  
+	}      
 
       position.x += constants::object_width;
-      if (position.x >= constants::object_width * level1.level_width) {
+      if (position.x >= constants::object_width * level.width) {
 	position.x = 0;
 	position.y += constants::object_height;
       }
      
     }
+
+    sdl2::copyToRenderer(renderer, textures[TextureType::Player], {static_cast<int>(player.rect.x),
+								   static_cast<int>(player.rect.y),
+								   static_cast<int>(player.rect.z),
+								   static_cast<int>(player.rect.w)});
     
     sdl2::renderScreen(renderer);
 
